2a08955e4ca26eaa08daf9df8481fe1e8e3fb22e
davidcarlin <david.carlin@barclays.com>
2023-02-21 00:00:04 +0000

    part 1 done

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-const {Restaurant} = require("./models/index")
-const {sequelize} = require("./db");
+const { Restaurant } = require("./models/index");
+const { sequelize } = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
-
+//TODO: Create your GET Request Route Below:
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log("Your server is listening on port " + port);
-})
+  sequelize.sync();
+  console.log("Your server is listening on port " + port);
+});
